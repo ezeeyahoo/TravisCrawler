bb8586eec9a4503bb4eab3504a40b48ecb077372
Eshant Gupta <eshant.gupta@ibm.com>
2021-01-11 18:14:30 +0000

    all fields are getting populated

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,5 @@ RUN pwd; \
     apk add --update;\
     apk --no-cache --update-cache; \
     apk add python3-dev build-base git; \
-    # ln -s /usr/include/locale.h /usr/include/xlocale.h;\
     python3 -m pip install -u pip setuptools; \
-    # pip install autopep8 requests flake8 bandit openpyxl google-api-python-client google-auth-httplib2 google-auth-oauthlib;
     pip install -r requirements.txt;
--- src/main.py
+++ src/main.py
@@ -8,16 +8,22 @@ import requests as rq
 from datetime import date, datetime as dt
 import argparse as ap
 from xml.dom import minidom
-
+from requests.api import request
 from requests.models import HTTPError
+import pprint
 
 
 # If modifying these scopes, delete the file token.pickle.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive',
+          'https://www.googleapis.com/auth/drive.file'
+          ]
+
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = "1ND3SnMHpbFYeNjvU9gDNIuCP3a2-Q_xzxbtyl18V_8s"
-SAMPLE_RANGE_NAME = "ubuntu2004!A2:P"
+SPREADSHEET_ID = "1ND3SnMHpbFYeNjvU9gDNIuCP3a2-Q_xzxbtyl18V_8s"
+READ_RANGE_NAME = "ubuntu2004!A2:P"
+WRITE_RANGE_NAME = "ubuntu2004!Q1:U"
 
 
 def call_request_github(url):
@@ -26,7 +32,8 @@ def call_request_github(url):
 
 def get_api_url(url):
     url_parts = url.split("/")
-    return "https://api.github.com/repos/" + url_parts[-2] + "/" + url_parts[-1]
+    return "https://api.github.com/repos/" + url_parts[-2] + "/" \
+                                           + url_parts[-1]
 
 
 def get_contributers_number(url):
@@ -40,7 +47,7 @@ def get_last_pr_update(url):
 
     r1 = call_request_github(url)
     r1.raise_for_status()
-    r2 = call_request_github(url + '?per_page=1&state=close')
+    r2 = call_request_github(url + "?per_page=1&state=close")
     r2.raise_for_status()
 
     try:
@@ -55,22 +62,24 @@ def get_last_pr_update(url):
         closed_pr_date = dt.strptime(
             r2.json()[0]['closed_at'], "%Y-%m-%dT%H:%M:%S%z").date()
     except IndexError:
-        # print(open_pr_date, closed_pr_date)
         return open_pr_date if closed_pr_date == "" else closed_pr_date
     finally:
-        return open_pr_date if closed_pr_date < open_pr_date else closed_pr_date
+        return open_pr_date if closed_pr_date < open_pr_date\
+            else closed_pr_date
 
 
 def get_last_commit_date(url):
     r = call_request_github(url)
-    return dt.strptime(r.json()[0]['commit']['committer']['date'], "%Y-%m-%dT%H:%M:%S%z").date()
+    return dt.strptime(r.json()[0]['commit']['committer']['date'],
+                       "%Y-%m-%dT%H:%M:%S%z").date().isoformat()
 
 
 def get_ghaction_circle(url):
-    i = 0
-    r1 = call_request_github(url+'/actions/workflows')
+    actions = False
+    circle = False
+    r1 = call_request_github(url+"/actions/workflows")
     r1.raise_for_status()
-    r2 = call_request_github(url+'/contents')
+    r2 = call_request_github(url+"/contents")
     r2.raise_for_status()
 
     try:
@@ -104,6 +113,7 @@ def getText(nodelist):
 
 
 def is_travis_enabled(url):
+    travis = False
     r = call_request_github(url)
     try:
         travis = any(item['name'] == '.travis.yml' for item in r.json())
@@ -113,8 +123,8 @@ def is_travis_enabled(url):
 
 
 def get_travis_build_failure_date(url):
-    if not is_travis_enabled(url):
-        return False
+    if not is_travis_enabled(url+'/contents'):
+        return "NA"
 
     r1 = rq.get(
         'https://api.travis-ci.com/'+'/'.join(url.split('/')[3:6])+'/builds',
@@ -133,12 +143,8 @@ def get_travis_build_failure_date(url):
             r2.raise_for_status()
             r = r2
         except HTTPError:
-            return False
+            return "NA"
 
-    # r = ET.parse(r.text).getroot()
-    # print(r.text)
-    # exit(0)
-    # r = ET.fromstring(r.content)
     feed = minidom.parseString(r.text)
     entries = feed.getElementsByTagName("entry")
     for entry in entries:
@@ -148,33 +154,24 @@ def get_travis_build_failure_date(url):
                 linebyline = [i.strip().strip('</p>')
                               for i in summary_string.strip().split('\n')]
                 return linebyline[-2].lstrip('Finished at: ')
+    return "NA"
 
 
 def get_github_api_data(url):
     api_url = get_api_url(url)
-    # No. of contributors:
     no_of_contributors = get_contributers_number(api_url)
-    # print(no_of_contributors)
-    # Last PR filed on
-    last_pr_date = get_last_pr_update(api_url + "/pulls")
-    # print(last_pr_date)
-    # last change applied  =>  ,
+    last_pr_date = get_last_pr_update(api_url + "/pulls").isoformat()
     last_commit_on = get_last_commit_date(api_url + "/commits")
-    # print(last_commit_on)
-    # any sign of a Circle CI or GitHub Actions config file,
     ghaction_circle = get_ghaction_circle(api_url)
-    # print(ghaction_circle)
-    # Last time successful Travis build failed...
-    # print(api_url.split('/'))
     last_failed_travis_build = get_travis_build_failure_date(api_url)
-    # print(last_failed_travis_build)
-    # write_row(no_of_contributors, last_pr_date, last_commit_on, ghaction_circle, last_failed_travis_build)
+    return [
+        no_of_contributors, last_pr_date, last_commit_on,
+        ghaction_circle, last_failed_travis_build
+    ]
 
 
 def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
+
     creds = None
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -200,27 +197,54 @@ def main():
     sheet = service.spreadsheets()
     result = (
         sheet.values()
-        .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=SAMPLE_RANGE_NAME)
+        .get(spreadsheetId=SPREADSHEET_ID, range=READ_RANGE_NAME)
         .execute()
     )
     values = result.get("values", [])
 
+    # write_data = []
+    write_data = [["No of Contributers", "Last PR updated", "Last commit",
+                   "GH actions and/or Circle CI", "Last Travis build failed"]]
     if not values:
         print("No data found.")
     else:
-        print("Package, URL")
-        for row in values:
+        print("Index, Package, URL")
+        for i, row in enumerate(values, 2):
             # Print columns A and E, which correspond to indices 0 and 3.
             # print("%s, %s" % (row[0], row[3]))
-            if row[3] != "NA":
-                get_github_api_data(row[3])
+            try:
+                if not row:
+                    # raise IndexError
+                    write_data.append(["", "", "", "", ""])
+                    continue
+                name = row[0]
+                url = row[3]
+                if url is not None and url.strip() != "":
+                    print(i, name, url)
+                    write_data.append(get_github_api_data(url))
+            except IndexError:
+                write_data.append(["", "", "", "", ""])
+                continue
+
+        body = {
+            "values": write_data
+        }
+
+        response = sheet.values().append(
+            spreadsheetId=SPREADSHEET_ID, range=WRITE_RANGE_NAME,
+            valueInputOption='RAW', insertDataOption='OVERWRITE',
+            body=body
+        ).execute()
+
+        print(response['updates']['updatedRows'])
 
 
 parser = ap.ArgumentParser('Get details of github projects\n')
 parser.add_argument('--username', '-u', type=str,
                     required=True, help='Github username')
 parser.add_argument('--token', '-t', type=str, required=True,
-                    help='Github generated token for API access (check github docs for api token)')
+                    help='Github token for API (check github docs for API)'
+                    )
 username = parser.parse_args().username
 token = parser.parse_args().token
 if __name__ == "__main__":
